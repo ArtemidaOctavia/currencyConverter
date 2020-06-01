c787cc1e74f0bd99988a3faf85dcd43669804122
ArtemidaOctavia <42804865+ArtemidaOctavia@users.noreply.github.com>
2020-06-01 14:34:27 +0300

    Updates

---
+++ precache-manifest.6623cb28d14aa526e010c3006e83145a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc36f8f0c2d6adae617c68d2474713c",
+    "url": "/currencyConverter/index.html"
+  },
+  {
+    "revision": "7a3db6bf6030ad362a56",
+    "url": "/currencyConverter/static/css/main.da8a3240.chunk.css"
+  },
+  {
+    "revision": "ad766d8f0c850d923294",
+    "url": "/currencyConverter/static/js/2.11175969.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/currencyConverter/static/js/2.11175969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3db6bf6030ad362a56",
+    "url": "/currencyConverter/static/js/main.c57a2e09.chunk.js"
+  },
+  {
+    "revision": "ce358742ce96b4d24368",
+    "url": "/currencyConverter/static/js/runtime-main.395a5c76.js"
+  }
+]);
