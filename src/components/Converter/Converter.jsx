43047896c76a5da13005f99c1c0406bf2ca486d6
ArtemidaOctavia <42804865+ArtemidaOctavia@users.noreply.github.com>
2020-06-02 14:00:56 +0300

    converted value now render on change of input

--- src/components/Converter/Converter.jsx
+++ src/components/Converter/Converter.jsx
@@ -2,32 +2,36 @@ import React from 'react';
 import styles from './Converter.module.css'
 
 export const Converter = (props) => {
-  return (
-    <div>
+  return <div>
 
-      <div><input onChange={(event) => {props.setSum(event.target.value)}} placeholder={'BYR'}></input> <span>to {props.currency ? props.currency : '...'} </span><span>{
+    <div><input onChange={(event) => {
+      props.setSum(event.target.value);
+      props.setConverted((event.target.value / props.courses[0][`${props.currency}_out`]).toFixed(2))
+    }} placeholder={'BYR'}/> <span>to {props.currency ? props.currency : '...'} </span><span>{
 
-        props.converted ? `= ${props.converted}` : ''
+      props.converted ? `= ${props.converted}` : ''
 
-      }</span></div>
+    }</span></div>
 
-      {props.courses ?
-        Object.entries(props.courses[0]).map((item, index) => {
-          if (index <= 16) {
-            let currency = index % 2 === 0 ? item[0].substring(0, 3) : '';
-            if (index % 2 === 0) {
-              return <div onClick={() => {props.setCurrency(currency)}} className={styles.button} key={index}>{currency}</div>
-            }
+    {props.courses ?
+      Object.entries(props.courses[0]).map((item, index) => {
+        if (index <= 16) {
+          let currency = index % 2 === 0 ? item[0].substring(0, 3) : '';
+          if (index % 2 === 0) {
+            return <div onClick={() => {
+              props.setCurrency(currency)
+            }} className={styles.button} key={index}>{currency}</div>
           }
-        }) : ''
-      }
+        }
+      }) : ''
+    }
+    <div>
       <div>
-        <div>
-          <button onClick={ () => props.setConverted(
-            (props.sum / props.courses[0][`${props.currency}_out`]).toFixed(2)
-          )}>convert</button>
-        </div>
+        <button onClick={() =>
+          props.setConverted((props.sum / props.courses[0][`${props.currency}_out`]).toFixed(2))
+        }>convert
+        </button>
       </div>
     </div>
-  )
+  </div>
 };
