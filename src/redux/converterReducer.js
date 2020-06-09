bd7be202ecc40812c8945ab75715836fc1404b71
ArtemidaOctavia <42804865+ArtemidaOctavia@users.noreply.github.com>
2020-06-09 15:03:18 +0300

    fixed incorrectly calculated currencies

--- src/redux/converterReducer.js
+++ src/redux/converterReducer.js
@@ -20,16 +20,30 @@ export const converterReducer = (state = initialState, action) => {
         }
 
       } else {
-        if (state.currency === 'RUB') {
-          return {
-            ...state,
-            converted: (action.value * 100).toFixed(2)
+        switch (state.currency) {
+          case 'RUB':
+            return {
+              ...state,
+              converted: (action.value * 100).toFixed(2)
+            };
+            case 'UAH':
+            return {
+              ...state,
+              converted: (action.value * 100).toFixed(2)
+            };
+          case 'SEK': {
+            return {
+              ...state,
+              converted: (action.value * 10).toFixed(2)
+            }
           }
+          default:
+            return {
+              ...state,
+              converted: action.value
+            };
         }
-        return {
-          ...state,
-          converted: action.value
-        };
+
       }
     case 'SET-SUM':
       return {
