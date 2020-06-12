2af8d4a38791a66509d8a1bf704cbb030a6ac66c
ArtemidaOctavia <42804865+ArtemidaOctavia@users.noreply.github.com>
2020-06-12 17:19:19 +0300

    include bootstrap and try some styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import styles from './Header.module.scss'
+import {Button, Container} from "react-bootstrap";
 
 export const Header = (props) => {
   props.setCourses('Минск');
   return (
+    <Container>
       <nav>
-        <NavLink className={styles.navlink} to={`/converter`}>Converter</NavLink>
-        <NavLink className={styles.navlink} to={`/courses`}>Courses</NavLink>
+        <NavLink className={styles.navlink} to={`/converter`}><Button variant={'secondary'}>Converter</Button></NavLink>
+        <NavLink className={styles.navlink} to={`/courses`}><Button variant={'secondary'}>Courses</Button></NavLink>
       </nav>
+    </Container>
   )
 };
