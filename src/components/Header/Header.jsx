fb8fdbd052cf6cbeab2b905a0e889d0e77bb11f7
ArtemidaOctavia <42804865+ArtemidaOctavia@users.noreply.github.com>
2020-06-15 12:32:55 +0300

    fixed header positioning

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import styles from './Header.module.scss'
-import {Button, Container} from "react-bootstrap";
+import {Button, Container, Nav} from "react-bootstrap";
 
 export const Header = (props) => {
   props.setCourses('Минск');
   return (
     <Container>
-      <nav>
+      <Nav className={'navbar navbar-expand-sm justify-content-center'}>
         <NavLink className={styles.navlink} to={`/converter`}><Button variant={'secondary'}>Converter</Button></NavLink>
         <NavLink className={styles.navlink} to={`/courses`}><Button variant={'secondary'}>Courses</Button></NavLink>
-      </nav>
+      </Nav>
     </Container>
   )
 };
