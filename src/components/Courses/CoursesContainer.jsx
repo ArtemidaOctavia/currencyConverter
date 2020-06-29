ed49c195458945336b7023212a151429fbdfef6b
ArtemidaOctavia <42804865+ArtemidaOctavia@users.noreply.github.com>
2020-06-29 16:00:12 +0300

    refactored courses component

--- src/components/Courses/CoursesContainer.jsx
+++ src/components/Courses/CoursesContainer.jsx
@@ -3,9 +3,23 @@ import {connect} from 'react-redux';
 import {Courses} from "./Courses";
 import {getCoursesThunk} from "../../redux/coursesReducer";
 
+const renderTableRows = (currencyDescription, index, currencies) => {
+  if (index <= 16 && (currencyDescription[1] !== '0.0000')) {
+    let currency = index % 2 === 0 ? currencyDescription[0].substring(0, 3) : '';
+    if (index % 2 === 0) {
+      return <tr key={index}>
+        <td>{currency}</td>
+        <td>{currencyDescription[1]}</td>
+        <td>{currencies[index + 1][1]}</td>
+      </tr>
+    }
+  }
+};
+
 const mapStateToProps = (state) => {
   return {
-      courses: state.courses.courses
+    courses: state.courses.courses,
+    renderTableRows: renderTableRows
   }
 };
 
